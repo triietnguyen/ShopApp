158b0e1727db90c117590506c36206439d6c32bc
Triet Nguyen <trietnguyenhoangminh@gmail.com>
2025-04-24 22:09:22 +0700

    done UserController and UserRepository

--- backend/shopapp/src/main/java/com/project/shopapp/controllers/UserController.java
+++ backend/shopapp/src/main/java/com/project/shopapp/controllers/UserController.java
@@ -2,7 +2,9 @@ package com.project.shopapp.controllers;
 
 import com.project.shopapp.dtos.UserDTO;
 import com.project.shopapp.dtos.UserLoginDTO;
+import com.project.shopapp.services.user.IUserService;
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
@@ -15,7 +17,9 @@ import java.util.List;
 
 @RestController
 @RequestMapping("${api.prefix}/users")
+@RequiredArgsConstructor
 public class UserController {
+    private final IUserService userService;
     @PostMapping("/register")
     public ResponseEntity<?> createUser(@Valid @RequestBody UserDTO userDTO, BindingResult result){
         try{
@@ -28,7 +32,8 @@ public class UserController {
             }
             if(!userDTO.getPassword().equals(userDTO.getRetypePassword())){
                 return ResponseEntity.badRequest().body("Password does not match");
-            };
+            }
+            userService.createUser(userDTO);
             return ResponseEntity.ok("Register successfully");
         } catch (Exception e){
             return ResponseEntity.badRequest().body(e.getMessage());
@@ -38,6 +43,7 @@ public class UserController {
     @PostMapping("login")
     public ResponseEntity<String> login(
             @Valid @RequestBody UserLoginDTO userLoginDTO){
-        return ResponseEntity.ok("Some token");
+        String token = userService.login(userLoginDTO.getPhoneNumber(), userLoginDTO.getPassword());
+        return ResponseEntity.ok(token);
     }
 }
