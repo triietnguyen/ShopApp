13ef432200583ab951c71df0ef2ccc15f16d44e0
Triet Nguyen <trietnguyenhoangminh@gmail.com>
2025-04-24 12:40:32 +0700

    done model Category and Product

---
+++ backend/shopapp/src/main/java/com/project/shopapp/models/BaseEntity.java
@@ -0,0 +1,34 @@
+package com.project.shopapp.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Data
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@MappedSuperclass
+public class BaseEntity {
+    @Column(name = "created_at")
+    private LocalDateTime createAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updateAt;
+
+    @PrePersist
+    protected void onCreate(){
+        createAt = LocalDateTime.now();
+        updateAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate(){
+        updateAt = LocalDateTime.now();
+    }
+}
